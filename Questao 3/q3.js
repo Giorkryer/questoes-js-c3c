b6e0790813915b742c5cbaacd55d75a228e82399
Giordanny Maciel <63190815+Giorkryer@users.noreply.github.com>
2022-08-26 16:10:12 -0300

    Add files via upload

---
+++ Questao 3/q3.js
@@ -0,0 +1,35 @@
+function gerarResult(nota) {
+    
+    let num = document.querySelector("#num");
+    nota = parseFloat(num.value);
+    let notaCorrigida = parseFloat(num.value);
+
+    notaCorrigida = arredondar(nota);
+
+    if (notaCorrigida < 40) {
+        document.getElementById("result").innerHTML=(`Aluno foi reprovado com nota ${notaCorrigida}`) 
+    } else if(notaCorrigida > 100){
+        document.getElementById("result").innerHTML=(`Nota inválida`)
+    }
+    else{
+        document.getElementById("result").innerHTML=(`Aluno foi aprovado com nota ${notaCorrigida}`)
+    }
+    
+}
+
+
+function arredondar (nota) {
+    if (nota < 38) {
+        return nota;
+    } else if (nota % 5 >= 3 ) {
+        return nota = nota + (5 - (nota % 5))
+    } else {
+        return nota = nota - (nota % 5)
+    }
+
+
+}
+
+
+
+
